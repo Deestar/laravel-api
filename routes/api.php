5a2a440bb3547222c054e39ec2281bbd60906644
Deestar <110073733+Deestar@users.noreply.github.com>
2023-06-06 09:14:52 -0700

    got an online remote db working

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get("/name", function () {
     return response("Doyin Api");
 });
+Route::post("/name", [TestController::class, "store"]);
